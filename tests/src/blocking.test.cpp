cd939bb3c18ed93ae7399aa2f7493ed1692b587f
Karel Burda <karel.burda@honeywell.com>
2018-04-13 17:10:40 +0200

    Initial test fixture for the blocking timer

--- tests/src/blocking.test.cpp
+++ tests/src/blocking.test.cpp
@@ -7,12 +7,29 @@
 
 namespace
 {
-TEST(blocking, construction)
+
+class blocking_test : public ::testing::Test
+{
+protected:
+    timers::blocking m_blocking_timer;
+};
+
+TEST(blocking_test_suite, construction)
 {
     ASSERT_NO_THROW(timers::blocking blocking_timer);
+}
 
-    timers::blocking blocking_timer;
+TEST_F(blocking_test, default_values)
+{
+    EXPECT_FALSE(m_blocking_timer.m_terminated_by_client);
+}
 
-    EXPECT_FALSE(blocking_timer.m_terminated_by_client);
+TEST_F(blocking_test, termination)
+{
+    EXPECT_NO_THROW(m_blocking_timer.terminate());
+
+    m_blocking_timer.m_terminated_by_client = true;
+    m_blocking_timer.terminate();
+    EXPECT_TRUE(m_blocking_timer.m_terminated_by_client);
 }
 }

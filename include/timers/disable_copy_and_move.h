b35cf92333edfeae336a46f829ce10aa5d0ab3a5
Karel Burda <karel.burda@honeywell.com>
2018-04-20 15:45:28 +0200

    Improved formatting

--- include/timers/disable_copy_and_move.h
+++ include/timers/disable_copy_and_move.h
@@ -8,6 +8,7 @@ struct disable_copy_and_move
 {
     disable_copy_and_move() = default;
     virtual ~disable_copy_and_move() = default;
+
     disable_copy_and_move(const disable_copy_and_move &) = delete;
     disable_copy_and_move(disable_copy_and_move &&) = delete;
     disable_copy_and_move & operator=(const disable_copy_and_move &) = delete;

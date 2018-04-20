e4545cf6cab740d94861a3ffc40c900d49a50434
Karel Burda <karel.burda@honeywell.com>
2018-04-20 10:56:28 +0200

    Improved file structure in tests

---
+++ tests/include/static_assertions.h
@@ -0,0 +1,13 @@
+#pragma once
+
+template<typename class_type>
+void assert_properties()
+{
+    static_assert(std::is_default_constructible<class_type>::value, "class is not default constructible (should be)");
+
+    static_assert(!std::is_move_constructible<class_type>::value, "class is move constructible (shouldn't be)");
+    static_assert(!std::is_move_assignable<class_type>::value, "blocking timer is move assignable (shouldn't be)");
+
+    static_assert(!std::is_copy_constructible<class_type>::value, "blocking timer is copy constructible (shouldn't be)");
+    static_assert(!std::is_copy_assignable<class_type>::value, "blocking timer is copy assignable (shouldn't be)");
+}

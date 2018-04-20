c2226fdd33c1a1d3ba09fa03ff32e23e1274e61d
Karel Burda <karel.burda@honeywell.com>
2018-04-20 15:47:03 +0200

    Added test that checks throwing from the periodic timer

--- tests/unit/src/periodic.test.cpp
+++ tests/unit/src/periodic.test.cpp
@@ -41,7 +41,7 @@ TEST(periodic_construction_destruction, basic_construction_destruction)
     burda::timers::testing::assert_construction_and_destruction<burda::timers::periodic>();
 }
 
-TEST_F(periodic_test, callback)
+TEST_F(periodic_test, callback_called)
 {
     auto caller = std::async(std::launch::async, [this]()
     {
@@ -53,6 +53,13 @@ TEST_F(periodic_test, callback)
     EXPECT_GE(m_counter, 5);
 
     m_periodic_timer.stop();
+}
+
+TEST_F(periodic_test, callback_throwing)
+{
+    EXPECT_THROW(m_periodic_timer.start(0s, std::bind(&periodic_test::callback, this)), timers::exceptions::time_period_is_zero);
+    EXPECT_THROW(m_periodic_timer.start(-1h, std::bind(&periodic_test::callback, this)), timers::exceptions::time_period_is_negative);
+    EXPECT_THROW(m_periodic_timer.start(3s, nullptr), timers::exceptions::callback_not_callable);
 }
 
 TEST_F(periodic_test, stop)
@@ -67,7 +74,7 @@ TEST_F(periodic_test, stop)
     EXPECT_NO_THROW(m_periodic_timer.stop());
     EXPECT_TRUE(m_periodic_timer.m_terminated);
 
-    for (size_t i = 0; i < 10; ++i)
+    for (size_t i = 0; i < 100; ++i)
     {
         EXPECT_NO_THROW(m_periodic_timer.stop());
     }

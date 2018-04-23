c5e2e7e3475e0c55fc6f38ff06e80ec722b31b24
Karel Burda <karel.burda@honeywell.com>
2018-04-23 16:14:37 +0200

    Improved formatting

--- tests/unit/include/time_utils.h
+++ tests/unit/include/time_utils.h
@@ -35,10 +35,12 @@ inline double round_to_seconds(const std::chrono::nanoseconds & ns)
 inline double measure_time(std::function<void()> action)
 {
     const auto start = timers::testing::clock::now();
+
     if (action)
     {
         action();
     }
+
     const auto end = timers::testing::clock::now();
 
     return timers::testing::round_to_seconds(end - start);

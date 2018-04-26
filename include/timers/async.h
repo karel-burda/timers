23118b733636a6db6daaf7a17d046db103a49561
Karel Burda <karel.burda@honeywell.com>
2018-04-26 09:15:09 +0200

    Static assertion in the 'async' timer, only periodic and single_shot timers may behave in an async way

--- include/timers/async.h
+++ include/timers/async.h
@@ -1,18 +1,25 @@
 #pragma once
 
-#include "timers/type_definitions.h"
-
 #include <future>
 #include <memory>
 #include <mutex>
+#include <type_traits>
+
+#include "timers/type_definitions.h"
 
 namespace burda
 {
 namespace timers
 {
+class periodic;
+class single_shot;
+
 template <typename underlying_timer>
 class async : public underlying_timer
 {
+static_assert(std::is_same<underlying_timer, periodic>::value || std::is_same<underlying_timer, single_shot>::value,
+              "Only periodic or single_shot timers are allowed to behave in an asynchronous way");
+
 public:
     bool start(time_interval interval, timers_callback callback) override
     {

77f3037d5575f74ae48853c3a1df9881b5edac38
Karel Burda <karel.burda@honeywell.com>
2018-04-26 09:05:13 +0200

    Fixed static assert messages in tests

--- tests/unit/include/static_assertions.h
+++ tests/unit/include/static_assertions.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 namespace burda
 {
 namespace timers
@@ -9,13 +11,18 @@ namespace testing
 template <typename class_type>
 void assert_properties()
 {
-    static_assert(std::is_default_constructible<class_type>::value, "class is not default constructible (should be)");
+    static_assert(std::is_default_constructible<class_type>::value,
+                  "Given class_type is not default constructible (should be)");
 
-    static_assert(!std::is_move_constructible<class_type>::value, "class is move constructible (shouldn't be)");
-    static_assert(!std::is_move_assignable<class_type>::value, "blocking timer is move assignable (shouldn't be)");
+    static_assert(!std::is_move_constructible<class_type>::value,
+                  "Given class_type is move constructible (shouldn't be)");
+    static_assert(!std::is_move_assignable<class_type>::value,
+                 "Given class_type is move assignable (shouldn't be)");
 
-    static_assert(!std::is_copy_constructible<class_type>::value, "blocking timer is copy constructible (shouldn't be)");
-    static_assert(!std::is_copy_assignable<class_type>::value, "blocking timer is copy assignable (shouldn't be)");
+    static_assert(!std::is_copy_constructible<class_type>::value,
+                 "Given class_type is copy constructible (shouldn't be)");
+    static_assert(!std::is_copy_assignable<class_type>::value,
+                 "Given class_type is copy assignable (shouldn't be)");
 }
 }
 }

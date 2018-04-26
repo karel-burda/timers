51c59cc07f2b690e715ccddb47889b0220cf23ea
Karel Burda <karel.burda@honeywell.com>
2018-04-26 16:24:28 +0200

    Setting the executable output dir for example and tests explicitly (because of consistence and CI)

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,4 +1,7 @@
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
+# Explicit setting to have it common on all platforms
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)

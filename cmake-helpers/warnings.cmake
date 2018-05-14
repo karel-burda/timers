b636c3be523a1be6d3d401dd2aebb3fe9ab5b9aa
Karel Burda <karel.burda@honeywell.com>
2018-05-14 22:04:18 +0200

    Re-factored cmake adding cxx flags

--- cmake-helpers/warnings.cmake
+++ cmake-helpers/warnings.cmake
@@ -1,34 +1,31 @@
+macro(add_cxx_compiler_flag FLAG)
+    if (NOT CMAKE_CXX_FLAGS MATCHES ${FLAG})
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAG}")
+    endif()
+endmacro()
+
 # Set pedantic warnings on all platforms
 # TODO: Remove code duplication
 if (MSVC)
     if (CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
         string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
     else()
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
+        add_cxx_compiler_flag("/W4")
     endif()
 
-    if (NOT CMAKE_CXX_FLAGS MATCHES "/Wall")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Wall")
-    endif()
-
-    if (NOT CMAKE_CXX_FLAGS MATCHES "/WX")
-        # Hardly usable on MSVC without excluding stdlib
-        #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
-    endif()
+    add_cxx_compiler_flag("/Wall")
+    # Hardly usable on MSVC without excluding stdlib
+    add_cxx_compiler_flag("/WX")
 else()
-    if (NOT CMAKE_CXX_FLAGS MATCHES "-Wall")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
-    endif()
-
-    if (NOT CMAKE_CXX_FLAGS MATCHES "-Wno-long-long")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-long-long")
-    endif()
-
-    if (NOT CMAKE_CXX_FLAGS MATCHES "-pedantic")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic")
-    endif()
-
-    if (NOT CMAKE_CXX_FLAGS MATCHES "-Werror")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
-    endif()
+     add_cxx_compiler_flag("-Wall")
+     add_cxx_compiler_flag("-Wno-long-long")
+     add_cxx_compiler_flag("-pedantic")
+     add_cxx_compiler_flag("-Werror")
 endif()
+
+macro(supress_cxx_compiler_warning WARNING)
+    if (MSVC)
+    else()
+        add_cxx_compiler_flag("-Wno-${WARNING}")
+    endif()
+endmacro()

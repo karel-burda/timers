48733f36ada56ab8ad2ded5592165d86c46bc24e
Karel Burda <karel.burda@honeywell.com>
2018-05-14 13:24:35 +0200

    Linking pthreads if available

--- cmake-helpers/pthreads.cmake
+++ cmake-helpers/pthreads.cmake
@@ -1,9 +1,7 @@
 # This macro is meant to be executed only after target is created
-macro(link_pthreads_if_needed)
-    if (CMAKE_COMPILER_IS_GNUXX OR CMAKE_COMPILER_IS_GNUCC)
-        message(STATUS "gnu compiler detected, trying to link pthreads")
-        find_package (Threads)
-        target_link_libraries (${PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
-    endif()
+macro(link_pthreads_if_available)
+    find_package (Threads QUIET)
+    message(STATUS "pthreads found, going to link these")
+    target_link_libraries (${PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
 endmacro()
 

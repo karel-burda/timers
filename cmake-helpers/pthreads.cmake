7da7f02fa6043c4252315ec48a10586b0b5d8783
Karel Burda <karel.burda@honeywell.com>
2018-05-16 15:56:01 +0200

    Fixed linking of pthreads when not found

--- cmake-helpers/pthreads.cmake
+++ cmake-helpers/pthreads.cmake
@@ -1,7 +1,10 @@
 # This macro is meant to be executed only after target is created
 macro(link_pthreads_if_available)
     find_package (Threads QUIET)
-    message(STATUS "pthreads found, going to link these")
-    target_link_libraries (${PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
+
+    if (Threads_FOUND)
+        message(STATUS "timers: pthreads found, going to link these")
+        target_link_libraries (${PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
+     endif()
 endmacro()
 

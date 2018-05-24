fafd870d84626a142dd07f435abcd1d8deaf3490
Karel Burda <karel.burda@honeywell.com>
2018-05-24 14:30:36 +0200

    Removed hard-coded seconds from the time utils in tests and using generic 'duration<double>'

--- tests/unit/include/time_utils.h
+++ tests/unit/include/time_utils.h
@@ -11,9 +11,9 @@ namespace testing
 {
 using clock = std::chrono::steady_clock;
 
-void assert_that_elapsed_time_in_tolerance(std::chrono::seconds elapsed, std::chrono::seconds lower_bound, std::chrono::seconds upper_bound);
+void assert_that_elapsed_time_in_tolerance(std::chrono::duration<double> elapsed, std::chrono::duration<double> lower_bound, std::chrono::duration<double> upper_bound);
 
-std::chrono::seconds measure_time(std::function<void()> action);
+std::chrono::duration<double> measure_time(std::function<void()> action);
 }
 }
 }
--- tests/unit/src/time_utils.cpp
+++ tests/unit/src/time_utils.cpp
@@ -10,13 +10,13 @@ namespace timers
 {
 namespace testing
 {
-void assert_that_elapsed_time_in_tolerance(const std::chrono::seconds elapsed, const std::chrono::seconds lower_bound, const std::chrono::seconds upper_bound)
+void assert_that_elapsed_time_in_tolerance(const std::chrono::duration<double> elapsed, const std::chrono::duration<double> lower_bound, const std::chrono::duration<double> upper_bound)
 {
     ASSERT_GE(elapsed, lower_bound);
     ASSERT_LE(elapsed, upper_bound);
 }
 
-std::chrono::seconds measure_time(std::function<void()> action)
+std::chrono::duration<double> measure_time(std::function<void()> action)
 {
     const auto start = clock::now();
 
@@ -31,7 +31,7 @@ std::chrono::seconds measure_time(std::function<void()> action)
 
     const auto end = clock::now();
 
-    return std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    return end - start;
 }
 }
 }

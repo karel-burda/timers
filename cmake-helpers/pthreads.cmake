24d453f76660b42714d03c36a2df0330079aacc5
Karel Burda <karel.burda@honeywell.com>
2018-05-28 14:59:21 +0200

    Fixed threading library macro (uses correctly 'CMAKE_THREAD_LIBS_INIT')

--- cmake-helpers/pthreads.cmake
+++ cmake-helpers/pthreads.cmake
@@ -1,9 +1,9 @@
 # This macro is meant to be executed only after target is created
 macro(_link_pthreads_if_available)
-    find_package (Threads QUIET)
+    find_package(Threads QUIET)
 
-    if (Threads_FOUND)
-        _print_status_message("pthreads found, going to link these")
-        target_link_libraries (${PROJECT_NAME} PUBLIC ${CMAKE_THREAD_LIBS_INIT})
+    if (CMAKE_THREAD_LIBS_INIT)
+        _print_status_message("threading library found, going to link: ${CMAKE_THREAD_LIBS_INIT}")
+        target_link_libraries(${PROJECT_NAME} PUBLIC ${CMAKE_THREAD_LIBS_INIT})
      endif()
 endmacro()

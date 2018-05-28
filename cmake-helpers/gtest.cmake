637517da342800b96ae06b98142b6ed33b21c344
Karel Burda <karel.burda@honeywell.com>
2018-05-28 14:58:04 +0200

    Improved build of the gtest

--- cmake-helpers/gtest.cmake
+++ cmake-helpers/gtest.cmake
@@ -1,25 +1,35 @@
 set(_GTEST_TARGET_NAME "gtest-build")
 set(_GTEST_BUILD_VARIANT "Release")
-set(_GTEST_SOURCE_DIR "gtest-src")
+set(_GTEST_SOURCE_DIR_NAME "gtest-src")
+set(_GTEST_SOURCE_DIR "${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR_NAME}")
 
 add_custom_target(${_GTEST_TARGET_NAME} SOURCES "")
 
 # We cannot use "ExternalProject_Add", since there's no options to disable submodules
-# that can cause some problems on some systems (e.g. this is buggy in Windows)
+# and this causes problems mainly on Windows git
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR}")
     _print_status_message("Cleaning the download dir for the ${_GTEST_TARGET_NAME}")
     file(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR})
 
-    add_custom_command(TARGET ${_GTEST_TARGET_NAME} PRE_BUILD COMMAND git clone https://github.com/google/googletest.git ${_GTEST_SOURCE_DIR} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-    add_custom_command(TARGET ${_GTEST_TARGET_NAME} PRE_BUILD COMMAND git checkout -f 0957cce368316577aae5ddfffcb67f24621d69e7 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR})
+    # TODO: perform a shallow clone on specific tag, but the gtest is currently not build-able on MSVC (1.7.0 and 1.8.0)
+    add_custom_command(TARGET ${_GTEST_TARGET_NAME} PRE_BUILD
+                       COMMAND git clone https://github.com/google/googletest.git ${_GTEST_SOURCE_DIR_NAME}
+                       WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    add_custom_command(TARGET ${_GTEST_TARGET_NAME} PRE_BUILD
+                       COMMAND git checkout -f 0957cce368316577aae5ddfffcb67f24621d69e7
+                       WORKING_DIRECTORY ${_GTEST_SOURCE_DIR})
 endif()
 
-add_custom_command(TARGET ${_GTEST_TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -DBUILD_GMOCK:BOOL=OFF -DBUILD_GTEST:BOOL=ON -DCMAKE_BUILD_TYPE=${_GTEST_BUILD_VARIANT} . WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR})
-add_custom_command(TARGET ${_GTEST_TARGET_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} --build . --config ${_GTEST_BUILD_VARIANT} WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR})
+add_custom_command(TARGET ${_GTEST_TARGET_NAME} POST_BUILD
+                   COMMAND ${CMAKE_COMMAND} -DBUILD_GMOCK:BOOL=OFF -DBUILD_GTEST:BOOL=ON -DCMAKE_BUILD_TYPE=${_GTEST_BUILD_VARIANT} .
+                   WORKING_DIRECTORY ${_GTEST_SOURCE_DIR})
+add_custom_command(TARGET ${_GTEST_TARGET_NAME} POST_BUILD
+                   COMMAND ${CMAKE_COMMAND} --build . --config ${_GTEST_BUILD_VARIANT}
+                   WORKING_DIRECTORY ${_GTEST_SOURCE_DIR})
 
-include_directories(SYSTEM ${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR}/googletest/include)
-link_directories(${PROJECT_NAME} ${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR}/googletest)
-link_directories(${PROJECT_NAME} ${CMAKE_BINARY_DIR}/${_GTEST_SOURCE_DIR}/googletest/${_GTEST_BUILD_VARIANT})
+include_directories(SYSTEM ${_GTEST_SOURCE_DIR}/googletest/include)
+link_directories(${PROJECT_NAME} ${_GTEST_SOURCE_DIR}/googletest)
+link_directories(${PROJECT_NAME} ${_GTEST_SOURCE_DIR}/googletest/${_GTEST_BUILD_VARIANT})
 
 macro(_add_compile_options_for_gtest)
     if (WIN32)

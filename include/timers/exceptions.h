7fa361fc0c9027782d179b52a9b256647eb3eaff
karel-burda <38255062+karel-burda@users.noreply.github.com>
2018-07-03 11:46:21 +0200

    Custom exceptions are inherited with public visibility (because of semantics and read-ability purposes)

--- include/timers/exceptions.h
+++ include/timers/exceptions.h
@@ -8,19 +8,19 @@ namespace timers
 {
 namespace exceptions
 {
-struct callback_not_callable : std::exception
+struct callback_not_callable : public std::exception
 {
 };
 
-struct time_period_is_invalid : std::exception
+struct time_period_is_invalid : public std::exception
 {
 };
 
-struct time_period_is_zero : time_period_is_invalid
+struct time_period_is_zero : public time_period_is_invalid
 {
 };
 
-struct time_period_is_negative : time_period_is_invalid
+struct time_period_is_negative : public time_period_is_invalid
 {
 };
 }
